814322d294fe36115da8fc89bb3e0f2469b1e9c4
Emmanuel Chukwu <officialecho95@outlook.com>
2023-05-09 13:35:04 +0000

    returns the number of actibe subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Function to query subscribers on a given Reddit subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
