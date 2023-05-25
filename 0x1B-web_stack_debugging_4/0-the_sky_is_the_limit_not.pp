4009610919573fc60cfc99ffdb8a28026f5e23c0
Emmanuel Chukwu <officialecho95@outlook.com>
2023-05-25 15:09:25 +0100

    handking more requests by nginx server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,11 @@
-/*
-this is a puppet script that clears the 943 errors of apache
-bench failed requests
-*/
-sudo apt-get install -y ruby
-sudo gem install puppet-lint -v 2.1.1
+# puppet script to increase the amount of traffic an Nginx server can handle
 
+exec { 'increase-ulimit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
